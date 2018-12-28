3121a70b0642513272c4bad841fb859f4f8f507a
KingFaiking <41138080+KingFaiking@users.noreply.github.com>
2018-12-28 22:36:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -317,21 +317,23 @@ client.on('message', message => {
         .setDescription(`**
 ..<اوامر الميوزك>..
 * --------------------------------------------- *
-'!&play', 'لتشغيل اغنية'
+'${prefix}play', 'لتشغيل اغنية'
 
-'!&join', 'دخول رومك الصوتي'
+'${prefix}join', 'دخول رومك الصوتي'
 
-'!&disconnect', 'الخروج من رومك الصوتي'
+'${prefix}disconnect', 'الخروج من رومك الصوتي'
 
-'!&skip', 'تخطي الأغنية'
+'${prefix}skip', 'تخطي الأغنية'
 
-'!&pause', 'ايقاف الاغنية مؤقتا'
+'${prefix}pause', 'ايقاف الاغنية مؤقتا'
 
-'!&resume', 'تكملة الاغنية'
+'${prefix}resume', 'تكملة الاغنية'
 
-'!&queue', 'اظهار قائمة التشغيل'
+'${prefix}queue', 'اظهار قائمة التشغيل'
 
-'!&np', 'اظهار الاغنية اللي انت مشغلها حاليا'
+'${prefix}np', 'اظهار الاغنية اللي انت مشغلها حاليا'
+
+'${prefix}vol', 'لرفع صوت لاغنيه'
 * --------------------------------------------- *
         **
         `)
@@ -375,5 +377,17 @@ client.on('message', message => {
     }, ms);1000
 
 });
+
+
+else if (command === `vol`) {
+		if (!msg.member.voiceChannel) return msg.channel.send('You are not in a voice channel!');
+		if (!serverQueue) return msg.channel.send('There is nothing playing.');
+		if (!args[1]) return msg.channel.send(`:loud_sound: Current volume is **${serverQueue.volume}**`);
+		serverQueue.volume = args[1];
+		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 5);
+		return msg.channel.send(`:speaker: تم تغير الصوت الي **${args[1]}**`);
+	} 
+
+
 
 client.login(process.env.BOT_TOKEN);
