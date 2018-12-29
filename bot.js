eeffbbb2e45e2f0573fa1bbc297853721a13cf33
KingFaiking <41138080+KingFaiking@users.noreply.github.com>
2018-12-29 14:22:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const conv = require('number-to-words')
 
-const prefix = '!&'
+const prefix = 'F'
 
 const client = new Discord.Client({ disableEveryone: true});
 
