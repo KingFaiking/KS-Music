08dfd50c7df80b9cc43e238ee9d96fd16eb12b33
KingFaiking <41138080+KingFaiking@users.noreply.github.com>
2018-12-30 19:15:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -470,7 +470,7 @@ client.on('message', message => {
   ` + '`' + prefix + 'setStatus` - **لتغيير حالة البوت**' + `
 
 
-** BetterGroip Shop..**`
+** KS SERVER..**`
   if(message.content === prefix + 'help') {
     message.author.send(helplist);
   }
